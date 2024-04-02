6cca4bf04349e190f70bb8827021661309723912
cugarte94 <cugarte@guiasyscoutschile.cl>
2024-04-02 12:38:19 -0300

    clase3

--- clase3/clase3_0_listas.py
+++ clase3/clase3_0_listas.py
@@ -2,11 +2,30 @@
 
 # Partamos con el siguiente problema, tengo que guardar en variables la informacion de 3 alumnos y sus respectivas 3 pruebas del semestre, esto se puede hacer de la siguiente manera:
 
+alumno1 = "Juan"
+alumno2 = "Pedro"
+alumno3 = "Maria"
+
+nota1_alumno1 = 5.5
+nota1_alumno2= 4.5
+nota1_alumno3 = 6.5
+
+nota2_alumno1 = 3.5
+nota2_alumno2= 5.5
+nota2_alumno3 = 7
+
+nota3_alumno1 = 4.5
+nota3_alumno2= 6.5
+nota3_alumno3 = 2.5
+
 # Como podemos ver, esto se puede hacer, pero es muy poco eficiente, ya que estamos repitiendo mucho codigo, y si tuvieramos que agregar mas alumnos, tendriamos que seguir repitiendo codigo, lo que no es una buena practica.
 
 # Para esto es que se crean las listas, las listas son un tipo de dato que nos permite almacenar varios valores en una sola variable, y acceder a ellos de manera mas facil.
 
 # Para crear una lista, se hace de la siguiente manera:
+
+lista_ejemplo = [1,2,3,4,5]
+print(lista_ejemplo)
 
 # Como podemos ver, una lista se crea poniendo los valores que queremos almacenar entre corchetes, y separandolos por comas, cada valor en la lista se le llama elemento.
 
@@ -14,13 +33,27 @@
 
 # Para acceder a un elemento de la lista, se hace de la siguiente manera:
 
+print(lista_ejemplo[0]) # 1
+print(lista_ejemplo[1]) # 2
+print(lista_ejemplo[2]) # 3
+print(lista_ejemplo[3]) # 4
+print(lista_ejemplo[4]) # 5
+# print(lista_ejemplo[5]) # IndexError
 
 # Como podemos ver, para acceder a un elemento de la lista, se pone el nombre de la lista, seguido de corchetes, y dentro de los corchetes el indice del elemento que queremos acceder.
 
 # Ahora, volviendo al problema anterior, podemos guardar la informacion de los alumnos y sus notas en listas de la siguiente manera:
 
+alumnos = ["Juan","Pedro","Maria","Juan","Pedro","Maria","Juan","Pedro","Maria","Juan","Pedro","Maria"]
+notas1 = [6,6.5,4.5,6,6.5,4.5,6,6.5,4.5,6,6.5,4.5]
+notas2 = [6,6.5,4.5,6,6.5,4.5,6,6.5,4.5,6,6.5,4.5]
+notas3 = [6,6.5,4.5,6,6.5,4.5,6,6.5,4.5,6,6.5,4.5]
+
 # Como podemos ver, ahora tenemos una lista para los nombres de los alumnos, y una lista para cada prueba, en donde cada elemento de la lista corresponde a la nota de un alumno en esa prueba.
 
 # Ahora, si queremos acceder a la informacion de un alumno en especifico, podemos hacerlo de la siguiente manera:
 
-
+print(alumnos[0])
+print(notas1[0])
+print(notas2[0])
+print(notas3[0])
--- clase3/clase3_1_ejercicicios.py
+++ clase3/clase3_1_ejercicicios.py
@@ -1,1 +1,17 @@
 # Vea el siguiente codigo y mejorelo usando listas para que ejecutarlo imprima lo mismo que el codigo original:
+
+equipo1 = "Barcelona"
+equipo2 = "Real Madrid"
+equipo3 = "Atletico de Madrid"
+
+goles_equipo1_partido1 = 3
+goles_equipo2_partido1 = 6
+goles_equipo3_partido1 = 2
+
+goles_equipo1_partido2 = 6
+goles_equipo2_partido2 = 6
+goles_equipo3_partido2 = 6
+
+goles_equipo1_partido3 = 2
+goles_equipo2_partido3 = 0
+goles_equipo3_partido3 = 2
