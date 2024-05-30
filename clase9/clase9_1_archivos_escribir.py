c10b08839811b1982574370a6d5c9b888e6cbffe
cugarte94 <cugarte@guiasyscoutschile.cl>
2024-05-30 13:28:22 -0400

    clase 9

--- clase9/clase9_1_archivos_escribir.py
+++ clase9/clase9_1_archivos_escribir.py
@@ -2,56 +2,18 @@
 
 # En este caso quiero crear un archivo nuevo llamado archivo2.txt y escribir la siguiente informacion en el: Cristobal,Ugarte,Nuñez
 
-archivo = open('clase9/archivo2.txt', 'w')
-
-archivo.write('Cristobal,Ugarte,Nuñez\n')
-
-archivo.close()
-
 # Ahora podemos hacer esto mismo para muchos datos
-
-archivo = open('clase9/archivo2.txt', 'w')
-archivo.write('Cristobal,Ugarte,Nuñez\n')
-archivo.write('Juan,Perez,Lopez\n')
-archivo.write('Pedro,Perez,Lopez\n')
-
-archivo.close()
 
 # Notese que el archivo se sobreescribe cada vez que se abre en modo escritura.
 
 # Ahora llenemos el archivo con datos de una encuesta
 
-archivo = open('clase9/archivo2.txt', 'w')
-
-archivo.write('Nombre,Apellido,Edad,Sexo\n')
-
-while True:
-    nombre = input('Ingrese su nombre: ')
-    apellido = input('Ingrese su apellido: ')
-    edad = input('Ingrese su edad: ')
-    sexo = input('Ingrese su sexo: ')
-    
-    archivo.write(f'{nombre},{apellido},{edad},{sexo}')
     # Agregamos el salto de linea para que la siguiente persona se escriba en la siguiente linea
-    archivo.write('\n')
+
     # Preguntamos si quiere seguir ingresando datos
-    continuar = input('Desea seguir ingresando datos? (si/no): ')
-    if continuar == 'no':
-        break
+    
 # Finalmente podemos hacer lo mismo desde una matriz
 
-archivo = open('clase9/archivo2.txt', 'w')
 
-matriz = [
-    ['Cristobal', 'Ugarte', 'Nuñez'], 
-    ['Juan', 'Perez', 'Lopez'], 
-    ['Pedro', 'Perez', 'Lopez']
-]
 
-for i in range(len(matriz)):
-    for j in range(len(matriz[i])):
-        archivo.write(matriz[i][j])
-        if j < len(matriz[i]) - 1:
-            archivo.write(',')
-    archivo.write('\n')
 
--- clase9_resuelta/clase9_0_archivos_leer.py
+++ clase9_resuelta/clase9_0_archivos_leer.py
@@ -8,23 +8,64 @@
 
 # Para leer un archivo usamos el metodo readline() que nos permite leer una linea del archivo, si se llama varias veces se iran leyendo las siguientes lineas.
 
+archivo = open('clase9/archivo1.txt', 'r')
+
+linea = archivo.readline()
+print(linea)
+linea = archivo.readline()
+print(linea)
+linea = archivo.readline()
+print(linea)
+
 # Cerremos el archivo
+archivo.close()
 
 # Ahora si queremos separar la informacion de cada linea podemos usar el metodo split() que nos permite separar un string en una lista de strings, por defecto separa por espacios, pero se le puede pasar un parametro para que separe por otro caracter.
+
+archivo = open('clase9/archivo1.txt', 'r')
+
+linea = archivo.readline()
 
 # Generalmente estos archivos contienen informacion separada por comas, en este caso la linea es "1,2,3,4,5,6,7,8,9,10\n" y queremos separarla por comas, pero antes debemos quitar el salto de linea que se genera al final de cada linea.
 # Usaremos el comando strip para quitar el salto de linea
 
+linea = linea.strip()
+
 
 # Usamos el comando split para separar la linea en una lista de strings, en este caso se separa por ","
 
+linea = linea.split(',')
+print(linea)
+
 # Repetimos el proceso para las siguientes lineas
 
+linea = archivo.readline()
+linea = linea.strip()
+linea = linea.split(',')
+print(linea)
 
 # Ahora el largo de la lista puede variar, por lo que si queremos acceder a un elemento en especifico debemos hacerlo por su indice
 
+print(linea[0])
+print(linea[1])
+print(linea[2])
 
 # Y ahora si queremos leer todo el archivo podemos usar un ciclo while
 
+archivo = open('clase9/archivo1.txt', 'r')
+
+matriz = []
+while True:
+    linea = archivo.readline()
+    if not linea:
+        break
+    linea = linea.strip()
+    linea = linea.split(',')
+    matriz.append(linea)
+
+for i in range(len(matriz)):
+    print(matriz[i])
+
 # Cerremos el archivo
 
+archivo.close()
